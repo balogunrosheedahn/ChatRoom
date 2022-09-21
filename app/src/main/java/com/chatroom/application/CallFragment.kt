18f4002747753e6d33999157429ec6a70b141928
Rosheedah Balogun <rosheedahbalo@gmail.com>
2022-09-21 18:39:53 +0100

    implement layout changes

--- app/src/main/java/com/chatroom/application/CallFragment.kt
+++ app/src/main/java/com/chatroom/application/CallFragment.kt
@@ -1,6 +1,18 @@
 package com.chatroom.application
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class CallFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_call,container, false)
+    }
 }
